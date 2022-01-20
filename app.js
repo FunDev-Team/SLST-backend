8a49c5d8fb5fead4715e64b90869d3df9068ef1d
Huu Loc <78517332+huynhhuuloc129@users.noreply.github.com>
2022-01-20 16:14:51 +0700

    update cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -6,23 +7,7 @@ const accountRouter = require('./routes/accountRoutes');
 const docRouter = require('./routes/docRoutes');
 const examRouter = require('./routes/examRoutes');
 
-app.all('/', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  next();
-});
-app.get('/', function (req, res, next) {
-    res.status(200).json({
-        status: 'success'
-    });
-});
-app.post('/', function (req, res, next) {
-  res.status(200).json({
-    status: 'success'
-    });
-});
-console.log('aaaaaaaaaaaa');
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/accounts', accountRouter);
 app.use('/api/docs', docRouter);
