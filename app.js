b2b7eadf0093fd62a5759d43b19d3a300ba9b7f8
Huu Loc <78517332+huynhhuuloc129@users.noreply.github.com>
2022-01-20 15:06:04 +0700

    Update COR

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const accountRouter = require('./routes/accountRoutes');
 const docRouter = require('./routes/docRoutes');
 const examRouter = require('./routes/examRoutes');
 
+app.all('/', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+app.get('/', function (req, res, next) {
+  // Handle the get for this route
+});
+app.post('/', function (req, res, next) {
+  // Handle the post for this route
+});
+
 app.use(express.json());
 app.use('/api/accounts', accountRouter);
 app.use('/api/docs', docRouter);
